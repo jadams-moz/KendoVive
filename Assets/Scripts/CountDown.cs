78ec9d0287d9f3018763a08bc181d0be5f200866
John Adams <jadams.moz@gmail.com>
2016-06-23 01:41:15 -0400

    Men strike demo
    Basic UI for selecting an exercise (hit it with shinai to select)
    Countdown timer and drum beat on hajime
    Dummy creates selected opening variable number of times

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountDown : MonoBehaviour {
+
+    TextMesh tm;
+    public float time = 3.0F;
+    bool paused = true;
+    bool played = false;
+    public AudioClip[] taiko;
+    private AudioSource taikoSource;
+
+    public AudioSource AddAudio()
+    {
+        AudioSource newAudio = gameObject.AddComponent<AudioSource>();
+        newAudio.loop = false;
+        newAudio.playOnAwake = false;
+        newAudio.volume = 1;
+        return newAudio;
+    }
+
+    // Use this for initialization
+    void Start () {
+        tm = GetComponent<TextMesh>();
+        //add audio components
+        taikoSource = AddAudio();
+
+        //load audio resources.
+        taikoSource = GetComponent<AudioSource>();
+        taiko = new AudioClip[] { (AudioClip)Resources.Load("Sounds/kokushikan_taiko") };
+        gameObject.GetComponent<Renderer>().enabled = false;
+    }
+
+    public void ToggleTimer()
+    {
+        paused = false;
+        played = false;
+        time = 3.0F;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (paused == false)
+        {
+            if (time > 1)
+            {
+                gameObject.GetComponent<Renderer>().enabled = true;
+                time -= Time.deltaTime;
+                tm.text = Mathf.RoundToInt(time).ToString();
+            }
+            else if (time > 0)
+            {
+                time -= Time.deltaTime;
+                gameObject.GetComponent<Renderer>().enabled = false;
+            }
+            else
+            {
+                if (played == false)
+                {
+                    taikoSource.clip = taiko[0];
+                    taikoSource.volume = 1;
+                    taikoSource.Play();
+                    played = true;
+                }
+                paused = true;
+                gameObject.GetComponent<Renderer>().enabled = false;
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/animTest.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animTest : MonoBehaviour {
+
+    GameObject bao;
+    GameObject rho;
+    GameObject lho;
+    GameObject rao;
+    GameObject txtcap;
+    public CountDown timerScript;
+    public float strikeCount;
+    
+    void Awake()
+    {
+        bao = GameObject.Find("spine_01");
+        lho = GameObject.Find("hand_l");
+        rho = GameObject.Find("hand_r");
+        rao = GameObject.Find("lowerarm_r");
+        txtcap = GameObject.Find("txtCapsule");
+
+    }
+
+    void OnTriggerEnter()
+    {
+        timerScript = GameObject.Find("txtCountDown").GetComponent<CountDown>();
+        timerScript.ToggleTimer();
+
+
+
+        StartCoroutine(WaitAndAnimate());
+         
+
+    }
+
+    void doTween(){     
+        iTween.RotateTo(bao,iTween.Hash(
+            "x",                    25,
+            "time",                 1.5F));
+        iTween.RotateAdd(rho,iTween.Hash(
+            "y",                    -15,
+            "x",                    -15,
+            "time",                 1.5F));
+        iTween.RotateAdd(lho, iTween.Hash(
+            "x",                    -30,
+            "time",                 1.5F));
+        iTween.RotateAdd(rao, iTween.Hash(
+            "y",                    20,
+            "x",                    -20,
+            "time",                 1.5F));
+        
+    }
+
+    void returnTween()
+    {
+        iTween.RotateTo(bao, iTween.Hash(
+            "x", 0,
+            "time", 1.5F));
+        iTween.RotateAdd(rho, iTween.Hash(
+            "y", 15,
+            "x", 15,
+            "time", 1.5F));
+        iTween.RotateAdd(lho, iTween.Hash(
+            "x", 30,
+            "time", 1.5F));
+        iTween.RotateAdd(rao, iTween.Hash(
+            "y", -20,
+            "x", 20,
+            "time", 1.5F));
+
+    }
+
+
+
+    IEnumerator WaitAndAnimate()
+    {
+        gameObject.GetComponent<Renderer>().enabled = false;
+        txtcap.GetComponent<Renderer>().enabled = false;
+        gameObject.GetComponent<Collider>().enabled = false;
+        yield return new WaitForSeconds(Random.Range(4, 6));
+
+        for (int i = 1; i <= strikeCount; i++)
+        {
+            doTween();
+            yield return new WaitForSeconds(Random.Range(2,5));
+            returnTween();
+            yield return new WaitForSeconds(Random.Range(3,5));
+        }
+        gameObject.GetComponent<Renderer>().enabled = true;
+        txtcap.GetComponent<Renderer>().enabled = true;
+        gameObject.GetComponent<Collider>().enabled = true;
+    }
+}
